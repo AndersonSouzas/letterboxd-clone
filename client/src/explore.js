309a8f015a4f1e8ffd04d8c460bc4732497c88d8
Anderson Souza <Anderson1231993@hotmail.com>
2024-12-25 20:49:44 -0300

    chore(client): create explore.js page structure
    - Created the basic structure of `explore.js` page.
    - Set up placeholder components for future content.

---
+++ client/src/explore.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from './api';
+
+function Explore() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        api.get('/movies/popular')
+            .then((response) => setMovies(response.data.results))
+            .catch((error) => console.error(error))
+    }, []);
+
+    return (
+        <div>
+            <h1>Explorar Filmes</h1>
+            <Link to="/register">Ir para Registro</Link>
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {movies.map((movie) => (
+                    <div key={movie.id} style={{ margin: '10px' }}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                            alt={movie.title}
+                            style={{ width: '150px', height: '225px' }}
+                        />
+                        <h3>{movie.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Explore;
