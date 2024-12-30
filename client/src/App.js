8b3efc4351a9a96e8ef653e81ac33e7bdf02a779
Anderson Souza <Anderson1231993@hotmail.com>
2024-12-30 20:45:05 -0300

    fix(client): change importations of App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Explore from './explore';
-import Register from './register';
+import Explore from './pages/explore';
+import Register from './pages/register';
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/" element={<Explore />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/pages/register" element={<Register />} />
       </Routes>
     </Router>
   );
