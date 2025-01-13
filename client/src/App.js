619f1037d5200cace1cde688194ee9d8fd492a2a
Anderson Souza <Anderson1231993@hotmail.com>
2025-01-13 15:39:54 -0300

    refactor(App.js): add routes to all pages using react-router-dom Link

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/home'
 import Explore from './pages/explore';
 import Register from './pages/register';
+import Login from './pages/login';
+import Profile from './pages/profile';
 
 function App() {
   return (
     <Router>
-      <Routes>
-      <Route path="/" element={<Explore />} />
-      <Route path="/pages/register" element={<Register />} />
-      </Routes>
+      <div>
+        <nav style={{ marginBottom: '20px', textAlign: 'center' }}>
+          <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
+          <Link to="/login" style={{ margin: '0 10px' }}>Login</Link>
+          <Link to="/profile" style={{ margin: '0 10px' }}>Perfil</Link>
+          <Link to="/Register" style={{ margin: '0 10px' }}>Registro</Link>
+          <Link to="/Explore" style={{ margin: '0 10px' }}>Explorar</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/explore" element={<Explore />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
